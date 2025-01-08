b2098e1a9165fc10773918339bdcebdeb680accc
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-01-08 14:10:33 +0000

    update for managers and team members

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
+from users.models import Team
 from .forms import TaskForm
 from .models import Task
 from django.contrib.auth.decorators import login_required
@@ -7,7 +9,11 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def task_list(request):
-    tasks = Task.objects.filter(user=request.user)
+    if request.user.groups.filter(name='Manager').exists():
+        teams = Team.objects.filter(manager=request.user)
+        tasks = Task.objects.filter(team__in=teams)
+    else:
+        tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 def create_task(request):
