f50c5415a67c56e9b77debb47afd9a2fd16eb3cf
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-02-05 21:15:27 +0000

    Deploy started

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -7,5 +8,9 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('home/', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
-     path('profile/settings/', views.profile_settings, name='profile_settings'),
+    path('profile/settings/', views.profile_settings, name='profile_settings'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
