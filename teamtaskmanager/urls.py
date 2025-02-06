1e2525f2dd43ed946a259475922fbc7f9583a039
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-02-06 21:22:05 +0000

    route to home

--- teamtaskmanager/urls.py
+++ teamtaskmanager/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/', include('users.urls')),
     path('tasks/', include('tasks.urls')),
-    path('', include('tasks.urls')),
+    path('', include('users.urls')),
 ]
