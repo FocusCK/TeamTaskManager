602ad4c85b7ce24dbf4b031f75dac9998a3723b1
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-02-08 16:06:40 +0000

    whitenoise

--- teamtaskmanager/settings.py
+++ teamtaskmanager/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "teamtaskmanager.urls"
@@ -135,6 +136,7 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tasks/static'),  # Point to the 'tasks/static' directory
