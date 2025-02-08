ac4c003ceab7c03cc50a4adc4fc071187dd9ff15
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-02-08 20:38:33 +0000

    settings

--- teamtaskmanager/settings.py
+++ teamtaskmanager/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-config = Config()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
@@ -86,7 +85,7 @@ WSGI_APPLICATION = "teamtaskmanager.wsgi.application"
 
 
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'))
+    'default': dj_database_url.parse(config('DATABASE_URL')) # type: ignore
 }
 
 
