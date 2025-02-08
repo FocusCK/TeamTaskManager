c926df3dff793abab64306cecd7ecd57acf0db94
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-02-08 17:24:27 +0000

    render

--- tasks/static/tasks/js/task_list.js
+++
@@ -1,8 +0,0 @@
-function confirmDelete(taskId) {
-    const confirmDeletion = confirm("Are you sure you want to delete this task?");
-    if (confirmDeletion) {
-        return true;
-    } else {
-        return false;
-    }
-    }
