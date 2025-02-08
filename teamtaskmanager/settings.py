3e7767a717264b9e8a8a20e8dda4a8ac04e7054b
Colm Kelleher <ckcolmkelleher@gmail.com>
2025-02-08 15:51:25 +0000

    render settings

--- teamtaskmanager/settings.py
+++ teamtaskmanager/settings.py
@@ -135,6 +135,11 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tasks/static'),  # Point to the 'tasks/static' directory
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
